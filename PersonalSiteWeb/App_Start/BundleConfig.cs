9e0e576ba9b1ac04f12446379b7035bd02447843
Kevy Augusto <kevyaugusto@gmail.com>
2017-06-24 23:17:18 +1000

    Massive update -change from PersonalSite app to PersonalSiteWeb mvc application -migrate all files for the new project -reduced images size -create bundle and minification

---
+++ PersonalSiteWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PersonalSiteWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/classie.js",
+                "~/Scripts/cbpAnimatedHeader.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/site").Include("~/Scripts/Site.js"));
+
+            //STYLES
+            bundles.Add(new StyleBundle("~/Content/styles")
+                .Include("~/Content/Css/bootstrap.css")
+                .Include("~/Content/Css/font-awesome.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/Content/site").Include("~/Content/Css/site.css", new CssRewriteUrlTransform()));
+            
+            //BundleTable.EnableOptimizations = false;
+        }
+    }
+}
---
+++ PersonalSiteWeb/Scripts/site.js
@@ -0,0 +1,9 @@
+﻿$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //$('#footerYear').text(new Date().getFullYear());
+
+    $(window).on("load resize", function() {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+});
